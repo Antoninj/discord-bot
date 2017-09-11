16c5e14d849f5e79c6f427298d9bf22db7f61031
Antonin Jousson <joussonantonin@gmail.com>
2017-09-11 17:30:19 +0200

    weather command behavior updated

--- moduls/weather.py
+++ moduls/weather.py
@@ -24,24 +24,31 @@ class Weather:
     async def weather(self, *location_name : str):
          """ weather forecast
          Nestor will show the weather forecast for a specific location.
-         eg. .weather paris
-        """
-        if location_name:
-            location_name = "+".join(keywords)
-            location_id = self.get_location_id(location_name)
+         eg. .weather paris"""
+         if location_name:
+            location_name = "_".join(location_name)
+            #location_id = self.get_location_id(location_name)
+            location_id = 6540125
 
-        else:
+         else:
             await self.bot.say("Please enter a location to get the weather forecast.")
             return
-        
-        url = "http://api.openweathermap.org/data/2.5/weather?id={}&APPID={}".format(location_id,API_KEY)
-            async with aiohttp.request('GET',url) as weather_request:
-                weather_data  = await weather_request.json()
-                print(weather_data)
 
-    def get_location_id(location_name):
+         url = "http://api.openweathermap.org/data/2.5/weather?id={}&APPID={}".format(location_id,API_KEY)
+         async with aiohttp.request('GET',url) as weather_request:
+            weather_data  = await weather_request.json()
+            wind_speed = weather_data["wind"]["speed"]
+            wind_speed_knots = self.convert_speed(wind_speed)
+            city_name = weather_data["name"]
+            await self.bot.say("Current wind speed in {}: {} knots.".format(city_name,wind_speed_knots))
+
+    def get_location_id(self,location_name):
         location_id = 1
         return location_id
+
+    def convert_speed(self,speed):
+        convertion_factor = 1.943844
+        return speed*convertion_factor
 
 def setup(bot):
      # Set up background tasks
